c6850c35c09cb1caae17df5116425e8b14564e56
Byoungjin Seok <sbj7534@seoultech.ac.kr>
2024-01-21 00:55:54 +0900

    experiment results update

--- train_simon_nr8_right_part3.py
+++
@@ -1,8 +0,0 @@
-import train_nets as tn
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
-
-for i in range(4):
-    input_diff = (0, 1 << (i+8))
-    print(f'simon nr8 - input_diff: {input_diff}')
-    net, h = tn.train_neural_distinguisher(num_epochs=200, num_rounds=8, depth=10, alg='simon_32_64', input_diff=input_diff, train_data_size=10**7, eval_data_size=10**6, verbose='0')
--- train_speck_nr5_explored_diff_hw2_2.py
+++
@@ -1,7 +0,0 @@
-import train_nets as tn
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-
-input_diff = (8192, 64)
-print(f'HW: 2, speck nr5 - input_diff: {input_diff}')
-net, h = tn.train_neural_distinguisher(num_epochs=200, num_rounds=5, depth=10, alg='speck_32_64', input_diff=input_diff, train_data_size=10**7, eval_data_size=10**6, verbose='1')
